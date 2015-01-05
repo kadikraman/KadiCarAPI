18ce34a478075198b6a9470e4be41f88cbf78758
Kadi Kraman <Kadi.Kraman@tessella.com>
2015-01-05 18:14:35 +0000

    Extracted partial html views, separated js into angular modules, added beginning of raw data view

---
+++ frontend/static/js/controllers.js
@@ -0,0 +1,32 @@
+(function(){
+
+	'use strict'
+
+	angular.module('kadiCarApp.controllers', [])
+		.controller('LineChartCtrl', ['$scope', '$http', function LineChartCtrl($scope, $http){
+
+			$scope.getAllExpenses = function(){
+				$http.get('/data/all_expenses')
+					.success(function(results){
+						// assign the result to a scope variable
+						$scope.data = results;
+					})
+			}
+			// wrapping this in an if statement to prevent continuous calls to this function
+			if(!$scope.data){
+				$scope.getAllExpenses();
+			}
+		}])
+		.controller('RawViewCtrl', ['$scope', function RawViewCtrl($scope){
+			$scope.expense =  {
+									id: 0,
+									expense_type: "initial_purchase",
+									amount: 1380,
+									expense_date: "2013-11-06",
+									mileage: 12345,
+									litres: 35,
+									comment: "this is a sample expense", 
+								};
+		}]);	
+
+})();
